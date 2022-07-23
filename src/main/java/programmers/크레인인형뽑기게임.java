dcab6ff4d2b9fe77c0c439301b5f9b85c78b1dca
JaejunSagong <jaejun7407@naver.com>
2022-07-23 13:11:35 +0900

    Changes

---
+++ src/main/java/programmers/크레인인형뽑기게임.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class 크레인인형뽑기게임 {
+
+    class Solution {
+        public int solution(int[][] board, int[] moves) {
+            int answer = 0;
+            ArrayList<Integer> answerList = new ArrayList<>();
+
+            for(int i=0; i< moves.length; i++){
+                for(int j=0; j< board.length; j++){
+                    if(board[j][moves[i]-1] != 0){
+                        answerList.add(board[j][moves[i]-1]);
+                        board[j][moves[i]-1] = 0;
+                        break;
+                    }
+                }
+//                System.out.println(answerList);
+            }
+            for(int i=0; i< answerList.size()-1; i++){
+                if(Objects.equals(answerList.get(i), answerList.get(i + 1))) {
+                    answerList.remove(i);
+                    answerList.remove(i);
+//                    System.out.println(answerList);
+                    i = -1;
+                    answer += 2;
+                    if (answerList.size() == 0) {
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
