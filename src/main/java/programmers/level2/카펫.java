da12ed249f76f978c1fc486b69829e64a3523cce
JaejunSagong <jaejun7407@naver.com>
2022-07-28 15:52:04 +0900

    카펫 추가

---
+++ src/main/java/programmers/level2/카펫.java
@@ -0,0 +1,21 @@
+package programmers.level2;
+
+public class 카펫 {
+    class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = new int[2];
+            double yellow1 = 2*yellow;
+            int i=1;
+            while(i>0) {
+                if (brown == yellow1 / i + 2*i+4) {
+                    answer[1] = i + 2;
+                    answer[0] = (brown + yellow) / answer[1];
+                    break;
+                }
+                i++;
+            }
+
+            return answer;
+        }
+    }
+}
