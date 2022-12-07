1a38a5b303314b412d859edb0784d39540af1a8b
JaejunSagong <jaejun7407@naver.com>
2022-12-07 13:22:17 +0900

    연속된수의합

---
+++ src/main/java/programmers/level0/연속된수의합.java
@@ -0,0 +1,22 @@
+package programmers.level0;
+
+public class 연속된수의합 {
+    class Solution {
+        public int[] solution(int num, int total) {
+            int[] answer = new int[num];
+            int a = 1;
+            int startNum;
+            int median = total / num;
+
+            if (num % 2 == 1) {
+                startNum = median - (num - 1) / 2;
+            } else {
+                startNum = median - (num - 2) / 2;
+            }
+            for (int i = 0; i < num; i++) {
+                answer[i] = startNum + i;
+            }
+            return answer;
+        }
+    }
+}
