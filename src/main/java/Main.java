95e74232de044b2a673de08ff01a45cd10a19780
JaejunSagong <jaejun7407@naver.com>
2022-12-17 11:49:24 +0900

    멀리뛰기 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import programmers.level1.다트게임;
+import programmers.level2.멀리뛰기;
 import programmers.level2.영어끝말잇기;
 
 public class Main {
     public static void main(String[] args) {
-        영어끝말잇기 solution = new 영어끝말잇기();
-        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
-        int[] answer = solution.solution(2,words);
-        System.out.println(answer[0] + answer[1]);
-
+        멀리뛰기 solution = new 멀리뛰기();
+        System.out.println(solution.solution(1));
     }
 }
