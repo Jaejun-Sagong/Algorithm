9cd94e213142dc9b180de47c283b0f322cf34dd4
JaejunSagong <jaejun7407@naver.com>
2022-12-25 16:13:53 +0900

    더맵게 추가

---
+++ src/main/java/programmers/level2/더맵게.java
@@ -0,0 +1,23 @@
+package programmers.level2;
+
+import java.util.PriorityQueue;
+
+public class 더맵게 {
+    class Solution {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+            for (int j : scoville) {
+                queue.add(j);
+            }
+            while(queue.peek() < K) {
+                if (queue.size() < 2)
+                    return -1;
+                queue.add(queue.poll() + (queue.poll() * 2));
+                answer++;
+            }
+            return answer;
+        }
+    }
+}
